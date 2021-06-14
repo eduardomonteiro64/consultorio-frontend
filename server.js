dbd14badec45291648484d6007794bdbbf132ec2
Marcos Manoel <marcosmss1304@gmail.com>
2021-06-14 16:07:28 -0300

    test deploy heroku 2

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
 const bodyParser = require("body-parser");
-const mailer = require("nodemailer");
 
 const port = process.env.PORT || 8080;
 const app = express();
