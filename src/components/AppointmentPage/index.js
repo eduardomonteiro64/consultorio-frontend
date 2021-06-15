cb20b72e2a4242e50a3c8f7fcfed71c760a318f8
eduardo.carm <eduardo.carmo@sbwebservicesext.net>
2021-06-15 19:12:29 -0300

    add appointmentpage

--- src/components/AppointmentPage/index.js
+++ src/components/AppointmentPage/index.js
@@ -1,20 +1,97 @@
 import React from "react";
-import { Layout, Row, Col, Typography, Input, List, Button } from "antd";
+import {
+  Layout,
+  Row,
+  Col,
+  Typography,
+  Input,
+  List,
+  Button,
+  Modal,
+  Form,
+  Drawer,
+} from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
+import axios from "axios";
+import MaskedInput from "antd-mask-input";
 
 const AppointmentPage = () => {
   const { Content } = Layout;
   const { Title } = Typography;
   const { Search } = Input;
 
-  const onSearch = (value) => console.log(value);
+  const [isModalVisible, setIsModalVisible] = React.useState(false);
 
-  const data = [
-    "10 Horas - Consulta com Paciente 01",
-    "11 Horas - Consulta com Paciente 02",
-    "12 Horas - Consulta com Paciente 03",
-    "13 Horas - Consulta com Paciente 04",
-    "14 Horas - Consulta com Paciente 05",
-  ];
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const [state, setState] = React.useState({
+    appointments: "",
+    userSchedule: "",
+  });
+
+  const [visible, setVisible] = React.useState(false);
+
+  const url =
+    "https://consultorio-backend0.herokuapp.com/api/userScheduleService";
+
+  const handleClick = (id) => {
+    axios.delete(`${url}/${id}`).then((resp) => resp);
+    alert("Consulta deletada com sucesso!");
+    window.location.reload();
+  };
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  const onFinish = (values) => {
+    axios
+      .post(`${url}`, {
+        time: values.time,
+        date: values.date,
+        name: values.name,
+        stateDocument: values.stateDocument,
+      })
+      .then((resp) => resp);
+    alert("Consulta incluida com sucesso!");
+    window.location.reload();
+  };
+
+  const onFinishFailed = () => {
+    alert("Faltam campos a serem preenchidos.");
+  };
+
+  const onSearch = (value) => {
+    axios.get(`${url}?stateDocument=${value}`).then((response) => {
+      setState({ ...state, userSchedule: response.data });
+      response.data && response.data.length >= 1
+        ? showDrawer()
+        : alert("Usuário não possui consultas agendadas.");
+    });
+  };
+
+  React.useEffect(() => {
+    const todayDate = new Date().toISOString().slice(0, 10);
+    axios.get(`${url}?date=${todayDate}`).then((response) => {
+      setState({ ...state, appointments: response.data });
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Content style={{ margin: "0 16px" }}>
       <div
@@ -23,26 +100,91 @@ const AppointmentPage = () => {
       >
         <Row>
           <Col sm={24} xs={24} align="middle">
-            <Title>Consultas</Title>
-            <Col sm={22} xs={24} align="middle">
-              <Search
-                placeholder="Digite o RG do paciente"
-                allowClear
-                enterButton="Buscar"
-                size={window.screen.width < 576 ? "small" : "large"}
-                onSearch={onSearch}
-                style={{ margin: 20 }}
-              />
-              {data && data.length > 1 ? (
+            <Title>Consultas do Dia</Title>
+            <Search
+              placeholder="Digite o documento do usuário"
+              allowClear
+              enterButton="Buscar"
+              size={window.screen.width < 576 ? "small" : "large"}
+              onSearch={onSearch}
+              style={{ margin: 20 }}
+            />
+            <Drawer
+              width={640}
+              placement="right"
+              closable={false}
+              onClose={onClose}
+              visible={visible}
+            >
+              {state && state.userSchedule.length >= 1 ? (
                 <List
-                  header={<div>Inicio do dia</div>}
-                  footer={<div>Fim do dia</div>}
                   bordered
-                  dataSource={data}
+                  dataSource={
+                    state && state.userSchedule ? state.userSchedule : ""
+                  }
                   renderItem={(item) => (
                     <List.Item>
-                      <Typography.Text mark>[01/01/2021]</Typography.Text>{" "}
-                      {item}
+                      <Typography style={{ padding: 10 }}>
+                        Dia da consulta: {item.date.substring(0, 10)}
+                      </Typography>{" "}
+                      <Typography style={{ padding: 10 }}>
+                        Hora da consulta: {item.time}
+                      </Typography>{" "}
+                      <Typography style={{ padding: 10 }}>
+                        <b>Nome do paciente: {item.name}</b>
+                      </Typography>{" "}
+                      <Typography style={{ padding: 10 }}>
+                        <Button
+                          icon={<DeleteOutlined />}
+                          onClick={() => handleClick(item._id)}
+                          type="primary"
+                          danger
+                        >
+                          Deletar Consulta
+                        </Button>
+                      </Typography>{" "}
+                    </List.Item>
+                  )}
+                />
+              ) : (
+                ""
+              )}
+            </Drawer>
+            <Col sm={22} xs={24} align="middle">
+              {state && state.appointments.length >= 1 ? (
+                <List
+                  header={
+                    <div>
+                      <b>Inicio do dia</b>
+                    </div>
+                  }
+                  footer={
+                    <div>
+                      <b>Fim do dia</b>
+                    </div>
+                  }
+                  bordered
+                  dataSource={
+                    state && state.appointments ? state.appointments : ""
+                  }
+                  renderItem={(item) => (
+                    <List.Item>
+                      <Typography style={{ padding: 10 }}>
+                        Hora da consulta: {item.time}
+                      </Typography>{" "}
+                      <Typography.Text mark style={{ padding: 10 }}>
+                        <b>Nome do paciente: {item.name}</b>
+                      </Typography.Text>{" "}
+                      <Typography style={{ padding: 10 }}>
+                        <Button
+                          icon={<DeleteOutlined />}
+                          onClick={() => handleClick(item._id)}
+                          type="primary"
+                          danger
+                        >
+                          Deletar Consulta
+                        </Button>
+                      </Typography>{" "}
                     </List.Item>
                   )}
                 />
@@ -51,7 +193,54 @@ const AppointmentPage = () => {
               )}
 
               <Col align="end" style={{ marginTop: 50 }}>
-                <Button type="primary">Adicionar Consulta</Button>
+                <Button type="primary" onClick={showModal}>
+                  Adicionar Consulta
+                </Button>
+                <Modal
+                  title="Adicionar Consulta"
+                  visible={isModalVisible}
+                  onOk={handleOk}
+                  onCancel={handleCancel}
+                  cancelButtonProps={{ style: { display: "none" } }}
+                  okButtonProps={{ style: { display: "none" } }}
+                >
+                  <Col>
+                    <Form
+                      name="basic"
+                      initialValues={{
+                        remember: true,
+                      }}
+                      onFinish={onFinish}
+                      onFinishFailed={onFinishFailed}
+                    >
+                      <Form.Item label="Nome" name="name">
+                        <Input />
+                      </Form.Item>
+                      <Form.Item label="RG" name="stateDocument">
+                        <MaskedInput mask="11.111.111-1" />
+                      </Form.Item>
+                      <Form.Item label="Horário" name="time">
+                        <input
+                          id="time"
+                          type="time"
+                          min="09:00"
+                          max="18:00"
+                          required
+                          pattern="[0-9]{2}:[0-9]{2}"
+                        ></input>
+                      </Form.Item>
+                      <Form.Item label="Dia" name="date">
+                        <MaskedInput
+                          mask="1111-11-11"
+                          placeholder="Ano/Mes/Dia"
+                        />
+                      </Form.Item>
+                      <Button type="primary" htmlType="submit">
+                        Enviar
+                      </Button>
+                    </Form>
+                  </Col>
+                </Modal>
               </Col>
             </Col>
           </Col>
